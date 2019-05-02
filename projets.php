c062b8c583e05d5a03febcff7bdedd080f0bd81b
spoitler <romain.bonnes@gmail.com>
2019-05-02 17:36:07 +0200

    bug taille ecran

--- projets.php
+++ projets.php
@@ -17,16 +17,17 @@
                   elit. Sed turpis ligula, aliquam vitae commodo ut,
                   egestas at nisi. Vivamus vulputate efficitur varius.
                   Donec at egestas lectus. Aliquam fringilla pretium
-                  euismod. In ac interdum ligula....</p>
+                  euismod. In ac interdum ligula....
+               </p>
+            </div>
+            <div class="chefProjet">
+               <h3>Chef de projet :</h3>
+               <p>&nbsp;Romain BONNES - I5</p>
+               <div class="containerMembers">
+                  <p><img src="img/membres.png" class="icone-membres" alt="icone membres" title="icone membres"></p>
+                  <p>5/7</p>
                </div>
-               <div class="chefProjet">
-                  <h3>Chef de projet :</h3>
-                  <p>&nbsp;Romain BONNES - I5</p>
-                  <div class="containerMembers">
-                     <p><img src="img/membres.png" class="icone-membres" alt="icone membres" title="icone membres"></p>
-                     <p>5/7</p>
-                  </div>
-               </div>
+            </div>
          </div>
       </div>
    </body>

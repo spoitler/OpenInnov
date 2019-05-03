d104d9f7695bede03d5c9d359074ef014e7f3a44
spoitler <romain.bonnes@gmail.com>
2019-05-03 12:22:20 +0200

    test 1 bloc taille telephone

--- projets.php
+++ projets.php
@@ -26,9 +26,9 @@
                <div class="chef-Projet">
                   <h3>Chef de projet :</h3>
                   <p>&nbsp;Romain BONNES - I5</p>
-                  <div class="container-Members">
+                  <div class="container-Membres">
                      <p><img src="img/membres.png" class="icone-membres" alt="icone membres" title="icone membres"></p>
-                     <p>5/7</p>
+                     <p class="nombres-membres">5/7</p>
                   </div>
                </div>
             </div>
@@ -50,9 +50,9 @@
                <div class="chef-Projet">
                   <h3>Chef de projet :</h3>
                   <p>&nbsp;Romain BONNES - I5</p>
-                  <div class="container-Members">
+                  <div class="container-Membres">
                      <p><img src="img/membres.png" class="icone-membres" alt="icone membres" title="icone membres"></p>
-                     <p>5/7</p>
+                     <p class="nombres-membres">5/7</p>
                   </div>
                </div>
             </div>
@@ -61,21 +61,23 @@
             <div class="title">
                <h2>Nom du projet</h2>
             </div>
-            <div class="description-projet">
-               <h3>Description :</h3>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit. Sed turpis ligula, aliquam vitae commodo ut,
-                  egestas at nisi. Vivamus vulputate efficitur varius.
-                  Donec at egestas lectus. Aliquam fringilla pretium
-                  euismod. In ac interdum ligula....
-               </p>
-            </div>
-            <div class="chefProjet">
-               <h3>Chef de projet :</h3>
-               <p>&nbsp;Romain BONNES - I5</p>
-               <div class="containerMembers">
-                  <p><img src="img/membres.png" class="icone-membres" alt="icone membres" title="icone membres"></p>
-                  <p>5/7</p>
+            <div class="sub-content-projet">
+               <div class="description-projet">
+                  <h3>Description :</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                     elit. Sed turpis ligula, aliquam vitae commodo ut,
+                     egestas at nisi. Vivamus vulputate efficitur varius.
+                     Donec at egestas lectus. Aliquam fringilla pretium
+                     euismod. In ac interdum ligula....
+                  </p>
+               </div>
+               <div class="chef-Projet">
+                  <h3>Chef de projet :</h3>
+                  <p>&nbsp;Romain BONNES - I5</p>
+                  <div class="container-Membres">
+                     <p><img src="img/membres.png" class="icone-membres" alt="icone membres" title="icone membres"></p>
+                     <p class="nombres-membres">5/7</p>
+                  </div>
                </div>
             </div>
          </div>

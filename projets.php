df885ff50cc848c74dc7fb3fb913965732cda7dd
spoitler <romain.bonnes@gmail.com>
2019-05-03 10:32:04 +0200

    test nouveaux design

--- projets.php
+++ projets.php
@@ -1,12 +1,60 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html>
    <head>
       <meta charset="utf-8">
       <title></title>
       <link rel="stylesheet" href="css/style.css">
    </head>
    <body>
-      <div class="main-container-projet">
+      <div class="main-container-projets">
+         <div class="container-projet">
+            <div class="title">
+               <h2>Nom du projet</h2>
+            </div>
+            <div class="sub-content-projet">
+               <div class="description-projet">
+                  <h3>Description :</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                     elit. Sed turpis ligula, aliquam vitae commodo ut,
+                     egestas at nisi. Vivamus vulputate efficitur varius.
+                     Donec at egestas lectus. Aliquam fringilla pretium
+                     euismod. In ac interdum ligula....
+                  </p>
+               </div>
+               <div class="chef-Projet">
+                  <h3>Chef de projet :</h3>
+                  <p>&nbsp;Romain BONNES - I5</p>
+                  <div class="container-Members">
+                     <p><img src="img/membres.png" class="icone-membres" alt="icone membres" title="icone membres"></p>
+                     <p>5/7</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="container-projet">
+            <div class="title">
+               <h2>Nom du projet</h2>
+            </div>
+            <div class="sub-content-projet">
+               <div class="description-projet">
+                  <h3>Description :</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                     elit. Sed turpis ligula, aliquam vitae commodo ut,
+                     egestas at nisi. Vivamus vulputate efficitur varius.
+                     Donec at egestas lectus. Aliquam fringilla pretium
+                     euismod. In ac interdum ligula....
+                  </p>
+               </div>
+               <div class="chef-Projet">
+                  <h3>Chef de projet :</h3>
+                  <p>&nbsp;Romain BONNES - I5</p>
+                  <div class="container-Members">
+                     <p><img src="img/membres.png" class="icone-membres" alt="icone membres" title="icone membres"></p>
+                     <p>5/7</p>
+                  </div>
+               </div>
+            </div>
+         </div>
          <div class="container-projet">
             <div class="title">
                <h2>Nom du projet</h2>

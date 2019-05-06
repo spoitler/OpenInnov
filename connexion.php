a975935d5be42838a82a7dc117f1ef05c620562d
spoitler <romain.bonnes@gmail.com>
2019-05-06 19:42:53 +0200

    debug page connexion

--- connexion.php
+++ connexion.php
@@ -3,7 +3,7 @@
 include_once ('functions.php');
 $auth = connection();
 if ($auth){
-   header("Location: projets.php?cn=$auth");
+   header('Location: projets.php?cn='.$auth);
 }else {
    header('Location: index.php?Error='.true);
 }

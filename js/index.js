ad615968bfe4ed52fb83fde07b9b37593ac1610d
spoitler <romain.bonnes@gmail.com>
2019-05-06 14:33:58 +0200

    fin design popup

--- js/index.js
+++ js/index.js
@@ -10,17 +10,21 @@ $(document).ready(function() {
       $('.test').delay(70).fadeOut(500);  //peut etre un delay la
      $('.popup-box').css("z-index","").removeClass('transform-in').addClass('transform-out');    //peut etre un delay la
      $('.popup-box1').css("z-index", "").removeClass('transform-in').addClass('transform-out');  //peut etre un delay la
+      $('.popup-box2').css("z-index", "").removeClass('transform-in').addClass('transform-out');  //peut etre un delay la
      $('.cache').fadeOut(500);
     e.preventDefault();
   });
 
   $('.popup-btn1').click(function(e) {
      $('.popup-box1').css("z-index", "1").removeClass('transform-out').addClass('transform-in');
-    $('.popup-wrap').fadeIn(500);
-    $('.test').delay(70).fadeIn(500);
-    $('.cache').fadeIn(500);
-
-
+     $('.test').delay(70).fadeIn(500);
+     $('.cache').fadeIn(500);
+    e.preventDefault();
+  });
+  $('.popup-btn2').click(function(e) {
+     $('.popup-box2').css("z-index", "1").removeClass('transform-out').addClass('transform-in');
+     $('.test').delay(70).fadeIn(500);
+     $('.cache').fadeIn(500);
     e.preventDefault();
   });
 });
--- projets.php
+++ projets.php
@@ -12,25 +12,27 @@
       <div class="main-container-projets">
          <div class="popup-btn1">
             <div class="container-projet popup-box1">
-               <div class="title">
-                  <h2>Nom du projet</h2>
-               </div>
-               <div class="sub-content-projet">
-                  <div class="description-projet">
-                     <h3>Description :</h3>
-                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing
-                        elit. Sed turpis ligula, aliquam vitae commodo ut,
-                        egestas at nisi. Vivamus vulputate efficitur varius.
-                        Donec at egestas lectus. Aliquam fringilla pretium
-                        euismod. In ac interdum ligula....
-                     </p>
+               <div class="sub-content">
+                  <div class="title">
+                     <h2>Nom du projet</h2>
                   </div>
-                  <div class="chef-Projet">
-                     <h3>Chef de projet :</h3>
-                     <p>&nbsp;Romain BONNES - I5</p>
-                     <div class="container-Membres">
-                        <p><img src="img/membres.png" class="icone-membres" alt="membres" title="icone membres"></p>
-                        <p class="nombres-membres">5/7</p>
+                  <div class="sub-content-projet">
+                     <div class="description-projet">
+                        <h3>Description :</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                           elit. Sed turpis ligula, aliquam vitae commodo ut,
+                           egestas at nisi. Vivamus vulputate efficitur varius.
+                           Donec at egestas lectus. Aliquam fringilla pretium
+                           euismod. In ac interdum ligula....
+                        </p>
+                     </div>
+                     <div class="chef-Projet">
+                        <h3>Chef de projet :</h3>
+                        <p>&nbsp;Romain BONNES - I5</p>
+                        <div class="container-Membres">
+                           <p><img src="img/membres.png" class="icone-membres" alt="membres" title="icone membres"></p>
+                           <p class="nombres-membres">5/7</p>
+                        </div>
                      </div>
                   </div>
                </div>
@@ -38,25 +40,27 @@
          </div>
          <div class="popup-btn">
             <div class="container-projet popup-box">
-               <div class="title">
-                  <h2>Nom du projet</h2>
-               </div>
-               <div class="sub-content-projet">
-                  <div class="description-projet">
-                     <h3>Description :</h3>
-                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing
-                        elit. Sed turpis ligula, aliquam vitae commodo ut,
-                        egestas at nisi. Vivamus vulputate efficitur varius.
-                        Donec at egestas lectus. Aliquam fringilla pretium
-                        euismod. In ac interdum ligula....
-                     </p>
+               <div class="sub-content">
+                  <div class="title">
+                     <h2>Nom du projet 2</h2>
                   </div>
-                  <div class="chef-Projet">
-                     <h3>Chef de projet :</h3>
-                     <p>&nbsp;Romain BONNES - I5</p>
-                     <div class="container-Membres">
-                        <p><img src="img/membres.png" class="icone-membres" alt="membres" title="icone membres"></p>
-                        <p class="nombres-membres">5/7</p>
+                  <div class="sub-content-projet">
+                     <div class="description-projet">
+                        <h3>Description :</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                           elit. Sed turpis ligula, aliquam vitae commodo ut,
+                           egestas at nisi. Vivamus vulputate efficitur varius.
+                           Donec at egestas lectus. Aliquam fringilla pretium
+                           euismod. In ac interdum ligula....
+                        </p>
+                     </div>
+                     <div class="chef-Projet">
+                        <h3>Chef de projet :</h3>
+                        <p>&nbsp;Romain BONNES - I5</p>
+                        <div class="container-Membres">
+                           <p><img src="img/membres.png" class="icone-membres" alt="membres" title="icone membres"></p>
+                           <p class="nombres-membres">5/7</p>
+                        </div>
                      </div>
                   </div>
                </div>
@@ -64,31 +68,32 @@
          </div>
          <div class="popup-btn2">
             <div class="container-projet popup-box2">
-               <div class="title">
-                  <h2>Nom du projet</h2>
-               </div>
-               <div class="sub-content-projet">
-                  <div class="description-projet">
-                     <h3>Description :</h3>
-                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing
-                        elit. Sed turpis ligula, aliquam vitae commodo ut,
-                        egestas at nisi. Vivamus vulputate efficitur varius.
-                        Donec at egestas lectus. Aliquam fringilla pretium
-                        euismod. In ac interdum ligula....
-                     </p>
+               <div class="sub-content">
+                  <div class="title">
+                     <h2>Nom du projet 3</h2>
                   </div>
-                  <div class="chef-Projet">
-                     <h3>Chef de projet :</h3>
-                     <p>&nbsp;Romain BONNES - I5</p>
-                     <div class="container-Membres">
-                        <p><img src="img/membres.png" class="icone-membres" alt="membres" title="icone membres"></p>
-                        <p class="nombres-membres">5/7</p>
+                  <div class="sub-content-projet">
+                     <div class="description-projet">
+                        <h3>Description :</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                           elit. Sed turpis ligula, aliquam vitae commodo ut,
+                           egestas at nisi. Vivamus vulputate efficitur varius.
+                           Donec at egestas lectus. Aliquam fringilla pretium
+                           euismod. In ac interdum ligula....
+                        </p>
+                     </div>
+                     <div class="chef-Projet">
+                        <h3>Chef de projet :</h3>
+                        <p>&nbsp;Romain BONNES - I5</p>
+                        <div class="container-Membres">
+                           <p><img src="img/membres.png" class="icone-membres" alt="membres" title="icone membres"></p>
+                           <p class="nombres-membres">5/7</p>
+                        </div>
                      </div>
                   </div>
                </div>
             </div>
          </div>
-      </div>
       <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
       <script  src="js/index.js"></script>
    </body>

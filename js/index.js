a2183df9e778d3f021bfa092021ed642d8b3ad98
spoitler <romain.bonnes@gmail.com>
2019-05-07 16:36:13 +0200

    modification popup pour etre au mileux de l'ecran tout le temps et mise en place du menu

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
    var id = "";
+   var pos = "";
   $('.popup-btn').click(function(e) {
      id = $(this).attr('id');
      // alert($(this).attr('id'));
      // alert('.popup-box'+id);
      $('.popup-box'+id).css("z-index", "1").delay(1000).removeClass('transform-out').addClass('transform-in');
-     $('.test').delay(70).fadeIn(500);
+     $('.overlay').delay(70).fadeIn(500);
      $('.cache'+id).fadeIn(500);
      e.preventDefault();
   });
 
   $('.popup-close').click(function(e) {
-     $('.test').delay(70).fadeOut(500);  //peut etre un delay la
+     $('.overlay').delay(70).fadeOut(500);  //peut etre un delay la
      $('.popup-box'+id).css("z-index","").removeClass('transform-in').addClass('transform-out');    //peut etre un delay la
      $('.cache'+id).fadeOut(70);
      e.preventDefault();
--- projets.php
+++ projets.php
@@ -14,7 +14,22 @@
       }
 
       ?>
-      <div class="test popup-close"></div>
+      <div class="container-menu">
+         <div class="container-icones">
+            <div class="icones">
+               <p><img src="img/list.png" alt="liste projets"></p>
+            </div>
+            <div class="icones">
+               <div class="icones-cross">
+                  <p><img src="img/add.png" alt="ajout projets"></p>
+               </div>
+            </div>
+            <div class="icones">
+               <p><img src="img/dark-project.png" alt="modification projet"></p>
+            </div>
+         </div>
+      </div>
+      <div class="overlay popup-close"></div>
       <div class="main-container-projets">
          <div class="popup-btn" id="1">
             <div class="container-projet popup-box popup-box1 transform-out">
@@ -78,11 +93,42 @@
                </div>
             </div>
          </div>
-         <div class="popup-btn" id="2">
-            <div class="container-projet popup-box popup-box2 transform-out">
+         <div class="popup-btn" id="3">
+            <div class="container-projet popup-box popup-box3 transform-out">
                <div class="sub-content">
                   <div class="title">
                      <h2>Nom du projet 3</h2>
+                     <div class="cache cache3">
+                        <a href="#">Postuler</a>
+                     </div>
+                  </div>
+                  <div class="sub-content-projet">
+                     <div class="description-projet">
+                        <h3>Description :</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                           elit. Sed turpis ligula, aliquam vitae commodo ut,
+                           egestas at nisi. Vivamus vulputate efficitur varius.
+                           Donec at egestas lectus. Aliquam fringilla pretium
+                           euismod. In ac interdum ligula....
+                        </p>
+                     </div>
+                     <div class="chef-Projet">
+                        <h3>Chef de projet :</h3>
+                        <p>&nbsp;Romain BONNES - I5</p>
+                        <div class="container-Membres">
+                           <p><img src="img/membres.png" class="icone-membres" alt="membres" title="icone membres"></p>
+                           <p class="nombres-membres">5/7</p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="popup-btn 2" id="2">
+            <div class="container-projet popup-box popup-box2 transform-out">
+               <div class="sub-content">
+                  <div class="title">
+                     <h2>Nom du projet 4</h2>
                      <div class="cache cache2">
                         <a href="#">Postuler</a>
                      </div>
@@ -109,6 +155,7 @@
                </div>
             </div>
          </div>
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
       <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
       <script  src="js/index.js"></script>
    </body>

6d02480b6fda13db5c63e2e64a86ec46263ae026
spoitler <romain.bonnes@gmail.com>
2019-05-21 10:22:42 +0200

    ca fonctionne toujours pas

--- connexion.php
+++ connexion.php
@@ -4,10 +4,10 @@ include ("functions.php");
 $auth = connection();
 $_SESSION['user']['cn'] = $auth;
 $bdd = getbdd();
-var_dump($bdd);
+// var_dump($bdd);
 $user = getUser($bdd,$auth);
-var_dump($user);
-echo $user;
+// var_dump($user);
+// echo $user;
 $_SESSION['user']['id'] = $user->id_utilisateur;
 if ($auth){
    header('Location: projets.php?cn='.$auth);
